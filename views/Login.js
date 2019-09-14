00773ca7522ed3ff02b1ac042a390f20b42ac43f
aulianar <arisky540@gmail.com>
2019-09-14 19:16:48 +0700

    add axios

--- views/Login.js
+++ views/Login.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Container, Button, Content, Form, Item, Input, Text} from 'native-base';
+import {ToastAndroid} from 'react-native';
+import axios from 'axios';
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      payload: {
+        email: '',
+        password: '',
+      },
+      respons: '',
+      showToast: false,
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   static navigationOptions = {
     title: 'Login',
     headerStyle: {
@@ -12,18 +27,58 @@ export default class Login extends Component {
       fontWeight: 'bold',
     },
   };
+  handleInput(name, value) {
+    this.setState(prevState => ({
+      payload: {
+        ...prevState.payload,
+        [name]: value,
+      },
+    }));
+  }
+  handleSubmit() {
+    let payload = {
+      ...this.state.payload,
+    };
+    axios
+      .post('http://reqres.in/api/login', payload)
+      .then(response => {
+        if (response.status === 200) {
+          return ToastAndroid.show(
+            'Login Success',
+            ToastAndroid.LONG,
+            ToastAndroid.TOP,
+            25,
+            50,
+          );
+        }
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <Container>
         <Content padder>
           <Form>
             <Item last>
-              <Input placeholder="Username" />
+              <Input
+                placeholder="Email"
+                onChangeText={value => {
+                  this.handleInput('email', value);
+                }}
+                value={this.state.payload.username}
+              />
             </Item>
             <Item last>
-              <Input placeholder="Password" secureTextEntry={true} />
+              <Input
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={value => {
+                  this.handleInput('password', value);
+                }}
+                value={this.state.payload.password}
+              />
             </Item>
-            <Button block style={styles.mt}>
+            <Button block style={styles.mt} onPress={this.handleSubmit}>
               <Text>Sign In</Text>
             </Button>
           </Form>
--- views/Register.js
+++ views/Register.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ToastAndroid} from 'react-native';
 import {Container, Button, Content, Form, Item, Input, Text} from 'native-base';
+import axios from 'axios';
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      payload: {
+        email: '',
+        password: '',
+      },
+      respons: '',
+      showToast: false,
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   static navigationOptions = {
     title: 'Register',
     headerStyle: {
@@ -12,18 +26,58 @@ export default class Register extends Component {
       fontWeight: 'bold',
     },
   };
+  handleInput(name, value) {
+    this.setState(prevState => ({
+      payload: {
+        ...prevState.payload,
+        [name]: value,
+      },
+    }));
+  }
+  handleSubmit() {
+    let payload = {
+      ...this.state.payload,
+    };
+    axios
+      .post('http://reqres.in/api/register', payload)
+      .then(response => {
+        if (response.status === 200) {
+          return ToastAndroid.show(
+            'Register Success',
+            ToastAndroid.LONG,
+            ToastAndroid.TOP,
+            25,
+            50,
+          );
+        }
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <Container>
         <Content padder>
           <Form>
             <Item last>
-              <Input placeholder="Username" />
+              <Input
+                placeholder="Email"
+                onChangeText={value => {
+                  this.handleInput('email', value);
+                }}
+                value={this.state.payload.username}
+              />
             </Item>
             <Item last>
-              <Input placeholder="Password" autoCompleteType="password" />
+              <Input
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={value => {
+                  this.handleInput('password', value);
+                }}
+                value={this.state.payload.password}
+              />
             </Item>
-            <Button block style={styles.mt}>
+            <Button block style={styles.mt} onPress={this.handleSubmit}>
               <Text>Sign Up</Text>
             </Button>
           </Form>
